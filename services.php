814c5aec585df2435604516e68ef3211ccf50d33
DianOffice <mdianzallo21@gmail.com>
2024-10-25 21:12:26 +0000

    'updated'

--- services.php
+++ services.php
@@ -97,11 +97,23 @@
             }
         }
 
+        @media (max-width: 768px) {
+            #navMenu button {
+                width: 95vw; /* Prend toute la largeur sur les petits écrans */
+            }
+        }
+
          .dropdown  li
         {
             background-color: rgb(240,240,240);
         }
         
+        #menu_tabStatic
+        {
+             height: 500px;
+             width: 600px;
+             border: 1px solid red;
+        }
 
     </style>
 </head>
@@ -170,7 +182,7 @@
     <div class="container mt-1" >
          <div class="d-flex align-items-start" >
             <div id="navMenu" class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
+                <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-statics" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
                         Tableau de Bord 
                 </button>
                  <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">
@@ -190,8 +202,10 @@
                 </button>
             </div>
             <div class="tab-content" id="v-pills-tabContent">
-                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
-                        
+                <div class="tab-pane fade show active" id="v-pills-statics" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
+                        <div  id="menu_tabStatic">
+                          
+                       </div>
                 </div>
                 <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
                        <div  id="">
@@ -207,6 +221,7 @@
     </div>
 
     <!-- Lien vers Bootstrap JS -->
+     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
